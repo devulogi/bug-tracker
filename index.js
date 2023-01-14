5790dca3476b6d7cfd5541d0b0d69ca9f2e91579
devulogi <devulogijrp.github@gmail.com>
2023-01-15 02:37:11 +0800

    add 404 handler and custom error handler

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!!' });
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 app.listen(port, () => {
   console.log(`Bugtracker app listening at http://localhost:${port}`);
   MongodbService();

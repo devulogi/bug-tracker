815f5985d5469f69b1d424bd32fa76ba4ef070e5
devulogi <devulogijrp.github@gmail.com>
2023-01-15 02:29:11 +0800

    added schemas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Express = require('express');
 const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const helmet = require('helmet');
+const cors = require('cors');
 const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
 const { port } = require('./configs');
@@ -11,11 +13,18 @@ const { RedisService } = require('./services/redis');
 const app = Express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(helmet());
 app.use(morgan('dev'));
+app.use(cors());
 app.use(Express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!!' });
+});
+
+app.get('/api/v1/users/:id', async (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: 'Hello World!!', userID: id });
 });
 
 app.listen(port, () => {

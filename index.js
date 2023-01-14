15b8b51d83f357859f723521d7e78f20a7d3be56
devulogi <devulogijrp.github@gmail.com>
2023-01-15 02:31:18 +0800

    Development (#9)
    * refactor path param type to string and added summary
    
    * added schemas
    
    * removed dummy route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Express = require('express');
 const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const helmet = require('helmet');
+const cors = require('cors');
 const swaggerDocument = YAML.load('./docs/openapi.yaml');
 
 const { port } = require('./configs');
@@ -11,7 +13,9 @@ const { RedisService } = require('./services/redis');
 const app = Express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(helmet());
 app.use(morgan('dev'));
+app.use(cors());
 app.use(Express.json());
 
 app.get('/', (req, res) => {

fdaa5ca33c673d922ff805e6292feaaae722673c
devulogi <devulogijrp.github@gmail.com>
2023-01-30 23:31:10 +0800

    update: remove unused imports

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,6 +1,5 @@
 const apiV1Router = require('express').Router();
 const { usersRouter } = require('./users');
-const { ErrorHandler } = require('../../helpers/ErrorHandler');
 
 apiV1Router.use('/users', usersRouter);
 
